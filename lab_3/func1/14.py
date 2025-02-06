072d4a4c33d1ee00c3b2b6199fd82815f1396daa
Dikossia <d_abdrakhman@kbtu.kz>
2025-02-06 15:05:39 +0500

    2 task

--- lab_3/func1/14.py
+++ lab_3/func1/14.py
@@ -1,21 +1,7 @@
-import math
+from sphere_volume import sphere_volume
+from sort_unique import unique_elements
 
-def sphere_volume(radius):
-    return (4 / 3) * math.pi * radius**3
+radius = int(input("Enter the radius: "))
+print(sphere_volume(radius))  
+print(unique_elements([1, 2, 2, 3, 4, 4, 5]))  
 
-def unique_elements(lst):
-    unique = []
-    for item in lst:
-        if item not in unique:
-            unique.append(item)
-    return unique
-
-def is_palindrome(string):
-    cleaned = ''.join(filter(str.isalnum, string)).lower()
-    return cleaned == cleaned[::-1]
-
-from func1 import sphere_volume, unique_elements, is_palindrome
-
-print(sphere_volume(5))  # Вывод: 523.5987755982989
-print(unique_elements([1, 2, 2, 3, 4, 4, 5]))  # Вывод: [1, 2, 3, 4, 5]
-print(is_palindrome("racecar"))  # Вывод: True
